cb797ed734cb6960053184b095c564d7c0109ed2
Greem <orangenewyorkcreativeagency@gmail.com>
2020-05-03 20:07:22 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,52 @@
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+
+  goal = {}
+
+  data.each do |color_gender_lives, value|
+
+    data[color_gender_lives].each do |each_color_gen_li, names|
+
+      #puts "each_color_gen_li #{each_color_gen_li}"
+
+      # name
+      counter = 0
+      while counter < names.length do
+        each_name = names[counter]
+
+        counter += 1
+
+        goal["#{each_name}"] = {}
+        goal["#{each_name}"][:colors] = []
+        goal["#{each_name}"][:gender] = []
+        goal["#{each_name}"][:lives] = []
+
+
+        #puts "names #{names} #{each_color_gen_li}"
+        #puts "each_name #{each_name} #{each_color_gen_li}"
+        #puts "goal.keys #{goal.keys}"
+
+
+        index= 0
+        while index < goal.keys.length do
+          each_goal_name = goal.keys[index]
+          if each_goal_name == each_name
+            goal["#{each_name}"][:lives] << each_color_gen_li
+          end
+          index += 1
+        end
+
+
+
+
+
+
+      end
+
+    end
+
+  end
+
+  return goal
+
 end
