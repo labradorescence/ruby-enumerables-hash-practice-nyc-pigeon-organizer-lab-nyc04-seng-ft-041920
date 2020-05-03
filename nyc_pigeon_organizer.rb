1506a4ada94094fb321129eee9e7cb3a0ed54b68
Greem <orangenewyorkcreativeagency@gmail.com>
2020-05-03 20:10:24 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,51 +2,54 @@
 def nyc_pigeon_organizer(data)
 
   goal = {}
-
   data.each do |color_gender_lives, value|
-
-    data[color_gender_lives].each do |each_color_gen_li, names|
-
-      #puts "each_color_gen_li #{each_color_gen_li}"
-
-      # name
-      counter = 0
-      while counter < names.length do
-        each_name = names[counter]
-
-        counter += 1
-
-        goal["#{each_name}"] = {}
-        goal["#{each_name}"][:colors] = []
-        goal["#{each_name}"][:gender] = []
-        goal["#{each_name}"][:lives] = []
-
-
-        #puts "names #{names} #{each_color_gen_li}"
-        #puts "each_name #{each_name} #{each_color_gen_li}"
-        #puts "goal.keys #{goal.keys}"
-
-
-        index= 0
-        while index < goal.keys.length do
-          each_goal_name = goal.keys[index]
-          if each_goal_name == each_name
-            goal["#{each_name}"][:lives] << each_color_gen_li
-          end
-          index += 1
-        end
-
-
-
-
-
-
+    value.each do |each_color_gen_li, name_arr|
+      name_arr.each do |name| #breaking down the original data
+       goal[name] = {:color => [], :gender => [], :lives => []} #putting structur in the goal
+       end
       end
-
     end
 
+  #inserting colors in the goal color array
+  goal_names = goal.keys
+    data[:color].each do |d_each_color, d_name_arr|
+      d_name_arr.each do |d_each_name|
+         #puts "d_each_name:::: #{d_each_name}"
+         goal_names.each do |g_each_name|
+          #puts "g_each_name #{g_each_name}"
+          if d_each_name === g_each_name ##why triple == ?
+          goal[d_each_name][:color] << d_each_color.to_s
+           end
+        end
+      end      
   end
 
+
+  goal_names = goal.keys
+    data[:gender].each do |d_each_gender, d_name_arr|
+      d_name_arr.each do |d_each_name|
+         #puts "d_each_name:::: #{d_each_name}"
+         goal_names.each do |g_each_name|
+          #puts "g_each_name #{g_each_name}"
+          if d_each_name === g_each_name ##why triple == ?
+          goal[d_each_name][:gender] << d_each_gender.to_s
+           end
+        end
+      end      
+  end
+
+  goal_names = goal.keys
+    data[:lives].each do |d_each_lives, d_name_arr|
+      d_name_arr.each do |d_each_name|
+         #puts "d_each_name:::: #{d_each_name}"
+         goal_names.each do |g_each_name|
+          #puts "g_each_name #{g_each_name}"
+          if d_each_name === g_each_name ##why triple == ?
+          goal[d_each_name][:lives] << d_each_lives.to_s
+           end
+        end
+      end      
+  end
   return goal
 
 end
